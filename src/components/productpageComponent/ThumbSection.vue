0b57c3ce7db8c24199abf0d8625fb862583eb2e0
David Bai <baiminghan@gmail.com>
2023-08-30 12:51:42 +1200

    temp setup products page

---
+++ src/components/productpageComponent/ThumbSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrapper">
+      <h1 class="text-4xl">Products</h1>
+      <p class="w-1/4 pt-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, qui illum. Laudantium at inventore reprehenderit hic rerum obcaecati perferendis iste.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  
+  .wrapper {
+      height:35rem;
+      background-image: url('../../assets/images/products-thumb.png');
+      background-size: 100% auto;
+      background-repeat: no-repeat;
+      padding-top:8rem;
+      padding-left:4rem;
+  }
+  
+  
+  
+  </style>
+  
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <Navbar/>
+    <ThumbSection/>
+
+    <div class="h-[80vh]">
+
+    </div>
+
+
     <PageFooter/>
   
   </div>
@@ -11,6 +18,7 @@
   
   import Navbar from '../components/Navbar.vue';
   import PageFooter from '../components/PageFooter.vue';
+import ThumbSection from '../components/productpageComponent/ThumbSection.vue';
   
   
   
