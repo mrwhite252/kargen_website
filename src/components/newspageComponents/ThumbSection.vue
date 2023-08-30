3f8595087eb4319510e32cf5a7bc0c7f36daa974
David Bai <baiminghan@gmail.com>
2023-08-30 15:00:35 +1200

    add the thumb section for news page

---
+++ src/components/newspageComponents/ThumbSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex px-12 py-16 justify-between">
+    <div class="left w-2/3">
+        <h1 class="text-5xl pb-2">News</h1>
+        <p class="">Get to know more about Kargen in news.</p>
+        <div class="flex border border-black justify-bewteen w-[20rem] mt-4">
+            <input type="text" class="w-[90%]" >
+            <img class="w-4 py-2" src="../../assets/images/icon/search.png" alt="">
+        </div>
+    </div>
+    <div class="right w-1/3 ml-auto">
+        <img class="w-[20rem]" src="../../assets/images/news-thumb.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
       <Navbar/>
+      <ThumbSection/>
+
       <PageFooter/>
     
     </div>
@@ -11,6 +13,7 @@
     
     import Navbar from '../components/Navbar.vue';
     import PageFooter from '../components/PageFooter.vue';
+import ThumbSection from '../components/newspageComponents/thumbSection.vue';
     
     
     
